a4d2b4c75554f047693bb61b43598bfc172c494f
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-11-28 10:35:45 -0500

    Initial Commit

---
+++ CodeUsingAStyleGuide/CodeUsingAStyleGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeUsingAStyleGuideApp.swift
+//  CodeUsingAStyleGuide
+//
+//  Created by Tyler Lawrence1 on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeUsingAStyleGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeUsingAStyleGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeUsingAStyleGuide
+//
+//  Created by Tyler Lawrence1 on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
